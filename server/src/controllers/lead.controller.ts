b1f605acdb08e650441e5222748f1e52208c79a2
Vivek <vivek17082004@gmail.com>
2026-05-24 14:51:42 +0530

    Fixed lead

--- server/src/controllers/lead.controller.ts
+++ server/src/controllers/lead.controller.ts
@@ -1,9 +1,19 @@
-import Lead from "../models/lead.model";
+import Lead from "../models/Lead";
+
+export const getLeads = async (req: any, res: any) => {
+  try {
+    const leads = await Lead.find();
+
+    res.json(leads);
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
 
 export const createLead = async (req: any, res: any) => {
   try {
-    console.log(req.body);
-
     const lead = await Lead.create({
       name: req.body.name,
       email: req.body.email,
@@ -17,7 +27,38 @@ export const createLead = async (req: any, res: any) => {
 
     res.status(500).json({
       message: "Server Error",
-      error,
+    });
+  }
+};
+
+export const updateLead = async (req: any, res: any) => {
+  try {
+    const lead = await Lead.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+      }
+    );
+
+    res.json(lead);
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+export const deleteLead = async (req: any, res: any) => {
+  try {
+    await Lead.findByIdAndDelete(req.params.id);
+
+    res.json({
+      message: "Lead deleted",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
     });
   }
 };
